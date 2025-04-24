755fa1125cd6648d69ec8d445c00fda3c97d0ec7
HarshitaRaotole <2024.harshita.raotole@ves.ac.in>
2025-04-24 22:29:10 +0530

    Initial commit of simplejavaapp

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Prints Hello, Java to the console
+        System.out.println("Hello, Java!");
+    }
+}
---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        // Simple test to check if the app is running
+        assertTrue(true);  // This will always pass
+    }
+}
